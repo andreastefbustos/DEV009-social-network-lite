80058c4f824cc1c04ec881955f47a4177d6c2a81
Andrea Bustos <andreastefbustos@gmail.com>
2023-08-03 01:29:13 +0200

    HU #1 Login user

--- src/main.js
+++ src/main.js
@@ -3,3 +3,12 @@
 import { myFunction } from './lib/index.js';
 
 myFunction();
+
+document.getElementById('search-icon').addEventListener('click', () => {
+  const searchInput = document.getElementById('search-input');
+  if (searchInput.style.display === 'none' || searchInput.style.display === '') {
+    searchInput.style.display = 'block';
+  } else {
+    searchInput.style.display = 'none';
+  }
+});
