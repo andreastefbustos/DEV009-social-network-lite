072c1518e16a2dd21a8d5cd8aeb6a4605b6de465
Andrea Bustos <andreastefbustos@gmail.com>
2023-08-14 10:01:01 -0400

    only user will be able to like when logged in

--- src/components/recipesUI.js
+++ src/components/recipesUI.js
@@ -1,7 +1,6 @@
 import { isUserLoggedIn } from './auth';
-import { showCustomAlert } from './showCustomAlert';
 
-export const createRecipeElement = (recipe, navigateTo) => {
+export const createRecipeElement = (recipe) => {
   const recipeElement = document.createElement('div');
   recipeElement.classList.add('recipe');
 
@@ -14,14 +13,16 @@ export const createRecipeElement = (recipe, navigateTo) => {
   const titleElement = document.createElement('h2');
   titleElement.textContent = recipe.title;
 
-  // Likes para la receta y evento
-  const likesCount = document.createElement('span');
-  likesCount.textContent = recipe.likes; // Inicialmente muestra el número de likes
+  recipeElement.append(img, titleElement);
 
-  const likeButton = document.createElement('i');
-  likeButton.className = 'fa-regular fa-heart'; // Clase para el corazon de FontAwesome
-  likeButton.addEventListener('click', () => {
-    if (isUserLoggedIn()) {
+  // Likes para la receta solo cuando el usuario este logeado
+  if (isUserLoggedIn()) {
+    const likesCount = document.createElement('span');
+    likesCount.textContent = recipe.likes; // Inicialmente muestra el número de likes
+
+    const likeButton = document.createElement('i');
+    likeButton.className = 'fa-regular fa-heart'; // Clase para el corazon de FontAwesome
+    likeButton.addEventListener('click', () => {
       if (likeButton.classList.contains('active')) {
         likeButton.classList.remove('active');
         // Aquí se maneja la lógica para deshacer el "me gusta"
@@ -34,13 +35,9 @@ export const createRecipeElement = (recipe, navigateTo) => {
         localStorage.setItem(`likes_${recipe.id}`, recipe.likes);
       }
       likesCount.textContent = recipe.likes; // Actualiza el texto del contador
-    } else {
-      showCustomAlert("Debes iniciar sesión para dar 'me gusta' a una receta.");
-      navigateTo('/login');
-    }
-  });
-
-  recipeElement.append(img, titleElement, likeButton, likesCount);
+    });
+    recipeElement.append(likeButton, likesCount);
+  }
 
   return recipeElement;
 };
