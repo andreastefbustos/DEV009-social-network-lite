a0ad6c3d1d1633fa761247076a6978e1ae7d4e40
Andrea Bustos <andreastefbustos@gmail.com>
2023-08-15 17:16:55 -0400

    like one user

--- src/components/recipesUI.js
+++ src/components/recipesUI.js
@@ -1,4 +1,5 @@
 import { isUserLoggedIn } from './auth';
+import { getLoggedInUser } from '../lib/index';
 
 export const createRecipeElement = (recipe) => {
   const recipeElement = document.createElement('div');
@@ -22,19 +23,38 @@ export const createRecipeElement = (recipe) => {
 
     const likeButton = document.createElement('i');
     likeButton.className = 'fa-regular fa-heart'; // Clase para el corazon de FontAwesome
+
+    // Obtener todos los likes de los usuarios
+    const allLikes = JSON.parse(localStorage.getItem('recipeLikes')) || {};
+
+    // Obtener el email del usuario logeado
+    const userEmail = getLoggedInUser().email;
+
+    // Verificar si el usuario actual ha dado "me gusta" a esta receta
+    if (allLikes[userEmail] && allLikes[userEmail].includes(recipe.id)) {
+      likeButton.classList.add('active');
+    }
+
     likeButton.addEventListener('click', () => {
       if (likeButton.classList.contains('active')) {
         likeButton.classList.remove('active');
         // Aquí se maneja la lógica para deshacer el "me gusta"
         recipe.likes -= 1;
-        localStorage.removeItem(`likes_${recipe.id}`);
+        // Remover el "me gusta" del usuario
+        const userLikes = allLikes[userEmail] || [];
+        const index = userLikes.indexOf(recipe.id);
+        if (index > -1) userLikes.splice(index, 1);
       } else {
         likeButton.classList.add('active');
         // Aquí se maneja la lógica para agregar el "me gusta"
         recipe.likes += 1;
-        localStorage.setItem(`likes_${recipe.id}`, recipe.likes);
+        // Agregar el "me gusta" del usuario
+        allLikes[userEmail] = allLikes[userEmail] || [];
+        allLikes[userEmail].push(recipe.id);
       }
-      likesCount.textContent = recipe.likes; // Actualiza el texto del contador
+      // Guardar de nuevo todos los likes
+      localStorage.setItem('recipeLikes', JSON.stringify(allLikes));
+      likesCount.textContent = recipe.likes;
     });
     recipeElement.append(likeButton, likesCount);
   }
--- src/lib/index.js
+++ src/lib/index.js
@@ -5,10 +5,17 @@ import {
   login as userLogin,
   register as registerPage,
   logout as userLogout,
+  getLoggedInUser as loggedInUser,
 } from './services';
 
 const login = (email, password) => userLogin(email, password);
 const register = (email, password) => registerPage(email, password);
 const logout = () => userLogout();
+const getLoggedInUser = () => loggedInUser();
 
-export { login, register, logout };
+export {
+  login,
+  register,
+  logout,
+  getLoggedInUser,
+};
