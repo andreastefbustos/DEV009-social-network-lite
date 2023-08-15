c5a5395b6156985b8d96a895bf22755c2901d55a
Andrea Bustos <andreastefbustos@gmail.com>
2023-08-15 18:46:58 -0400

    Modification likes

--- src/components/recipesUI.js
+++ src/components/recipesUI.js
@@ -25,23 +25,26 @@ export const createRecipeElement = (recipe) => {
     likeButton.className = 'fa-regular fa-heart'; // Clase para el corazon de FontAwesome
 
     // Obtener todos los likes de los usuarios
-    const allLikes = JSON.parse(localStorage.getItem('recipeLikes')) || {};
+    const initialAllLikes = JSON.parse(localStorage.getItem('recipeLikes')) || {};
 
     // Obtener el email del usuario logeado
     const userEmail = getLoggedInUser().email;
 
     // Verificar si el usuario actual ha dado "me gusta" a esta receta
-    if (allLikes[userEmail] && allLikes[userEmail].includes(recipe.id)) {
+    if (initialAllLikes[userEmail] && initialAllLikes[userEmail].includes(recipe.id)) {
       likeButton.classList.add('active');
     }
 
     likeButton.addEventListener('click', () => {
+      // Obtener todos los likes de los usuarios
+      const currentAllLikes = JSON.parse(localStorage.getItem('recipeLikes')) || {};
+
       if (likeButton.classList.contains('active')) {
         likeButton.classList.remove('active');
         // Aquí se maneja la lógica para deshacer el "me gusta"
         recipe.likes -= 1;
         // Remover el "me gusta" del usuario
-        const userLikes = allLikes[userEmail] || [];
+        const userLikes = currentAllLikes[userEmail] || [];
         const index = userLikes.indexOf(recipe.id);
         if (index > -1) userLikes.splice(index, 1);
       } else {
@@ -49,11 +52,11 @@ export const createRecipeElement = (recipe) => {
         // Aquí se maneja la lógica para agregar el "me gusta"
         recipe.likes += 1;
         // Agregar el "me gusta" del usuario
-        allLikes[userEmail] = allLikes[userEmail] || [];
-        allLikes[userEmail].push(recipe.id);
+        currentAllLikes[userEmail] = currentAllLikes[userEmail] || [];
+        currentAllLikes[userEmail].push(recipe.id);
       }
       // Guardar de nuevo todos los likes
-      localStorage.setItem('recipeLikes', JSON.stringify(allLikes));
+      localStorage.setItem('recipeLikes', JSON.stringify(currentAllLikes));
       likesCount.textContent = recipe.likes;
     });
     recipeElement.append(likeButton, likesCount);
